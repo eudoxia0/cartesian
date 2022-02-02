7a101df8c2ce2747ac934482e2ad54d8bac4d626
Fernando Borretti <fernando@borretti.me>
2022-02-02 08:47:34 +0000

    Give bundles a predictable path

--- build-and-copy-frontend.sh
+++ build-and-copy-frontend.sh
@@ -6,3 +6,5 @@ cd ..
 rm -rf server/theatre/static
 mkdir server/theatre/static
 cp -a frontend/build/. server/theatre/static
+mv server/theatre/static/static/css/main.*.css server/theatre/static/bundle.css
+mv server/theatre/static/static/js/main.*.js server/theatre/static/bundle.js
